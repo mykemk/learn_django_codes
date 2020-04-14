0f4b0ce7158af800fe0c1ecb9980222fbdbab321
Myke <mykemitrichev@gmail.com>
2020-04-14 08:46:00 +0300

    created travello app

--- learn/learn/urls.py
+++ learn/learn/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('', include('calc.urls')),
+    path('', include('travello.urls')),
     path('admin/', admin.site.urls),
 
 ]
